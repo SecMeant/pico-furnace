c8e0ab0c0bc404088e49e045919e392bce550a3c
maj0r <kornelu123@github.com>
2024-01-02 11:28:25 +0100

    Change format and getting sdk path by shell variables in configure_wifi.sh
    Formatting of file has been changed to improve file navigation.
    We would like to use shell variables for getting the sdk paths,
    due to necessity of changing the configure_wifi.sh file for
    different users.

--- configure_wifi.sh
+++ configure_wifi.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-cmake -B build -S . -DCMAKE_EXPORT_COMPILE_COMMANDS=Y -GNinja -DPICO_BOARD=pico_w -DPICO_SDK_PATH=/home/holz/etc/git/pico-sdk/ -DPICO_EXTRAS_PATH=/home/holz/etc/git/pico-extras/ -DWIFI_SSID=$(awk -F '=' '$1=="ssid" {print $2}' wlan.ini) -DWIFI_PASSWORD=$(awk -F '=' '$1=="pass" {print $2}' wlan.ini)
+cmake -B build -S . -DCMAKE_EXPORT_COMPILE_COMMANDS=Y -GNinja -DPICO_BOARD=pico_w \
+-DPICO_SDK_PATH=${PICO_SDK_PATH} -DPICO_EXTRAS_PATH=${PICO_EXTRAS_PATH} \
+-DWIFI_SSID=$(awk -F '=' '$1=="ssid" {print $2}' wlan.ini) \
+-DWIFI_PASSWORD=$(awk -F '=' '$1=="pass" {print $2}' wlan.ini)
