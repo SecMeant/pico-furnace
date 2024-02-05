7646ae4a90b0d45a5947e9a68bfaeeda63b03754
maj0r <kornelu123@github.com>
2024-02-05 01:39:08 +0100

    Changing message printed via TCP
    We would like to use different software, which
    allows us to controll the furnace via HTTP.

--- furnace.c
+++ furnace.c
@@ -30,8 +30,6 @@
 #define FURNACE_FIRE_PWM_SLICE pwm_gpio_to_slice_num(FURNACE_FIRE_PIN)
 #define FURNACE_FIRE_PWM_CHANNEL pwm_gpio_to_channel(FURNACE_FIRE_PIN)
 
-#define MAX_TEMP 1250
-
 typedef struct {
   struct tcp_pcb* server_pcb;
   struct tcp_pcb* client_pcb;
@@ -183,17 +181,26 @@ command_handler(furnace_context_t* ctx, uint8_t* buffer, void (*feedback)(const 
     const size_t msg_len = get_logs(msg, ctx->log_bits);
     feedback(msg, msg_len);
   } else if(memcmp(buffer, "help\n", 5) == 0) {
-    const char msg[] = "help             shows this message\n"
-                        "reboot           reboot device\n"
-                        "pwm <0;50>       sets pwm\n"
-                        "pwm              prints current pwm level\n"
-                        "temp <0;1250>    sets wanted temperature\n"
-                        "temp             shows current wanted temperature\n"
-                        "auto <0;1>       sets automatic pwm control, it is\n"
-                        "                 reaching temperature set by 'temp' command\n"
-                        "                 0 - off\n"
-                        "                 1 - on\n"
-                        "auto             shows current auto status\n";
+
+    const char msg[] = "help              \t\t shows this message\n"
+                        "reboot            \t\t reboot device\n"
+                        "pwm <0;50>        \t\t sets pwm\n"
+                        "pwm               \t\t prints current pwm level\n"
+                        "temp <0;1250>     \t\t sets wanted temperature\n"
+                        "temp              \t\t shows current wanted temperature\n"
+                        "auto <0;1>        \t\t sets automatic pwm control, it is\n"
+                        "                  \t\t reaching temperature set by 'temp' command\n"
+                        "                  \t\t\t 0 - off\n"
+                        "                  \t\t\t 1 - on\n"
+                        "auto              \t\t shows current auto status\n"
+                        "log <option> <0;1>\t\t sets output level on stdio\n"
+                        "                  \t\t\t options:\n"
+                        "                  \t\t\t\t server,\n"
+                        "                  \t\t\t\t thermocouple,\n"
+                        "                  \t\t\t\t basic\n"
+                        "                  \t\t\t 0 - off\n"
+                        "                  \t\t\t 1 - on\n"
+                        "log               \t\t prints names of turned on log options\n";
     const size_t msg_len = sizeof(msg)-1;
     feedback(msg, msg_len);
   }
@@ -215,7 +222,6 @@ static err_t
 tcp_server_recv(void* ctx_, struct tcp_pcb* tpcb, struct pbuf* p, err_t err)
 {
   furnace_context_t *ctx = (furnace_context_t*)ctx_;
-  unsigned arg;
 
   if (!p) {
     tcp_server_close(ctx);
@@ -352,26 +358,6 @@ do_tcp_work(furnace_context_t *ctx, bool deadline_met)
   }
 
   if (ctx->tcp.client_pcb && deadline_met) {
-    const int temperature_str_len = snprintf(
-      temperature_str,
-      sizeof(temperature_str),
-      "temp:%d, pwm:%u/%u\n",
-      ctx->cur_temp,
-      ctx->pwm_level,
-      MAX_PWM
-    );
-
-    tcp_server_send_data(
-      ctx,
-      ctx->tcp.client_pcb,
-      (uint8_t*)temperature_str,
-      temperature_str_len
-    );
-  }
-
-}
-
-  if (ctx->tcp.client_pcb && deadline_met) {
     const int temperature_str_len = format_status(temperature_str, ctx);
 
     tcp_server_send_data(
@@ -472,111 +458,6 @@ do_stdio_work(furnace_context_t* ctx, bool deadline_met)
     log_stdout_basic(ctx->log_bits, buffer);
   }
 
-  while(1) {
-    uint8_t c = getchar_timeout_us(0);
-
-    if(c == (uint8_t) PICO_ERROR_TIMEOUT) return;
-
-    if(ctx->stdio.parser == ctx->stdio.buffer + BUF_SIZE){
-      printf("\nLines longer than %d are invalid!\nResetting stdio buffer.\n", BUF_SIZE);
-      reset_stdio_data(ctx);
-      return;
-    }
-
-    // User may change last sent character from lf to cr, vice versa or crlf
-    // Although, in parsing command, we do not care how user sent this
-    // and we are always putting '\n' at the end of command
-    if(c == '\n' || c == '\r') {
-      *ctx->stdio.parser = '\n';
-      stdio_command_handler(ctx);
-      reset_stdio_data(ctx);
-      return;
-    }
-
-    *ctx->stdio.parser = c;
-    ctx->stdio.parser++;
-  }
-}
-
-static inline int
-sgn(int val_1, int val_2)
-{
-  if(val_1 > val_2)
-    return -1;
-  return 1;
-}
-
-static inline uint8_t
-clamp_u8(int min, int max, int val)
-{
-  uint8_t value = (uint8_t)val;
-
-  if(val < min)
-    value = min;
-
-  if(val > max)
-    value = max;
-
-  return value;
-}
-
-static void
-do_pilot_work(furnace_context_t *ctx)
-{
-  const bool deadline_met = get_absolute_time() > ctx->pilot.pilot_deadline;
-
-  if(deadline_met && ctx->pilot.is_enabled){
-    const int diff = ctx->cur_temp - ctx->pilot.last_temp;
-    const int sign = sgn(ctx->cur_temp, ctx->pilot.des_temp);
-    unsigned pwm = ctx->pwm_level;
-
-    ctx->pilot.last_temp = ctx->cur_temp;
-    ctx->pilot.pilot_deadline = make_timeout_time_ms(15000);
-
-    if(diff >= -5 && diff <= 1)
-      pwm += sign;
-
-    set_pwm_safe(ctx, pwm);
-  }
-}
-
-void
-init_pilot(furnace_context_t *ctx)
-{
-  ctx->pilot.des_temp = 0;
-  ctx->pilot.last_temp = 0;
-  ctx->pilot.is_enabled = false;
-}
-
-static void
-init_stdio(furnace_context_t* ctx)
-{
-  ctx->stdio.parser = ctx->stdio.buffer;
-  memset(&ctx->stdio.buffer, 0, BUF_SIZE);
-}
-
-static void
-reset_stdio_data(furnace_context_t* ctx)
-{
-  memset(&ctx->stdio.buffer, 0, BUF_SIZE);
-  ctx->stdio.parser = ctx->stdio.buffer;
-}
-
-static void
-stdio_command_handler(furnace_context_t* ctx)
-{
-  void
-  send_stdio(const char* msg, const size_t msg_len)
-  {
-    printf(msg);
-  }
-
-  command_handler(ctx, ctx->stdio.buffer, &send_stdio);
-}
-
-void
-do_stdio_work(furnace_context_t* ctx)
-{
   while(1) {
     uint8_t c = getchar_timeout_us(0);
 
