cc3e9eae832643b975cfab7a2a667cc85baa3203
maj0r <kornelu123@github.com>
2024-02-09 19:38:52 +0100

    bugfix: not passing the CONFIG_HOSTNAME variable to cmake
    Bug resulted in empty hostname.

--- configure_wifi.sh
+++ configure_wifi.sh
@@ -4,4 +4,5 @@ cmake -B build -S . -DCMAKE_EXPORT_COMPILE_COMMANDS=Y -GNinja -DPICO_BOARD=pico_
 -DCMAKE_BUILD_TYPE=Release \
 -DPICO_SDK_PATH=${PICO_SDK_PATH} -DPICO_EXTRAS_PATH=${PICO_EXTRAS_PATH} \
 -DWIFI_SSID=$(awk -F '=' '$1=="ssid" {print $2}' wlan.ini) \
--DWIFI_PASSWORD=$(awk -F '=' '$1=="pass" {print $2}' wlan.ini)
+-DWIFI_PASSWORD=$(awk -F '=' '$1=="pass" {print $2}' wlan.ini) \
+-DCONFIG_HOSTNAME=${CONFIG_HOSTNAME}
