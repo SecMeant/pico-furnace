443b9a8f1c8fb90d21de25f649ec5a1853b102e8
SecMeant <secmeant@gmail.com>
2024-03-15 23:06:27 +0100

    Add CONFIG_MAX_PWM for configuring MAX_PWM
    Hotplate requires binary PWM, meaning MAX_PWM=1.

--- common.h
+++ common.h
@@ -3,7 +3,7 @@
 #define CONSTEVAL_HEADER "consteval_header.h"
 
 #define MAX_TEMP 1100
-#define MAX_PWM 50U
+#define MAX_PWM ((unsigned int)(CONFIG_MAX_PWM))
 #define MAX_AUTO 1
 
 #define FORMAT_STATUS_FMT "temp:%d/%d, pwm:%u/%u, auto:%d\n"
